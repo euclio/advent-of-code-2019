b09ced3e70bc161aa66fea7e71d1b71b01f791d2
Andy Russell <arussell123@gmail.com>
2019-12-17 17:06:40 -0500

    add solution for day 4 part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -30,32 +30,73 @@ fn valid_password(n: u32) -> bool {
     has_double
 }
 
+fn contains_run_of_exactly_two(n: u32) -> bool {
+    let digits = digits(n);
+
+    let mut last_digit = digits[0];
+    let mut chain_len = 1;
+
+    for &digit in &digits[1..] {
+        if last_digit == digit {
+            chain_len += 1;
+        } else if chain_len == 2 {
+            return true;
+        } else {
+            chain_len = 1;
+        }
+
+        last_digit = digit;
+    }
+
+    return chain_len == 2;
+}
 
 fn main() {
     let num_passwords = (372037..=905157)
         .filter(|&n| valid_password(n))
         .count();
 
-    println!("{}", num_passwords);
+    println!("part 1: {}", num_passwords);
+
+    let num_passwords = (372037..=905157)
+        .filter(|&n| valid_password(n) && contains_run_of_exactly_two(n))
+        .count();
+
+    println!("part 2: {}", num_passwords);
 }
 
 #[cfg(test)]
 mod tests {
-    use super::valid_password;
+    use super::{contains_run_of_exactly_two, valid_password};
 
     #[test]
-    fn example1() {
+    fn part1_example1() {
         assert!(valid_password(111111));
     }
 
     #[test]
-    fn example2() {
+    fn part1_example2() {
         assert!(!valid_password(223450));
 
     }
 
     #[test]
-    fn example3() {
+    fn part1_example3() {
         assert!(!valid_password(123789));
+    }
+
+    #[test]
+    fn part2_example1() {
+        assert!(contains_run_of_exactly_two(112233));
+    }
+
+    #[test]
+    fn part2_example2() {
+        assert!(!contains_run_of_exactly_two(123444));
+    }
+
+    #[test]
+    fn part2_example3() {
+        assert!(contains_run_of_exactly_two(111122));
     }
 }
